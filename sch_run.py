a87315271c42085d1db32bc4e2c416a0bd1480bb
Igor Clark <igor@artrabbit.com>
2020-09-25 18:11:58 +0100

    rm pipfile.lock, rm logging

--- sch_run.py
+++ sch_run.py
@@ -1,14 +1,9 @@
 from sch_simulation.helsim_RUN_KK import SCH_Simulation
 
-print ("hello 1")
 df = SCH_Simulation(paramFileName='SCH-high_adult_burden.txt', demogName='UgandaRural', numReps=10)
-print ("hello 2")
 
 df.to_json('sch_results.json')
-print ("hello 3")
 
 df.plot(x='Time', y=['SAC Prevalence', 'Adult Prevalence'])
-print ("hello 4")
 
 df.plot(x='Time', y=['SAC Heavy Intensity Prevalence', 'Adult Heavy Intensity Prevalence'])
-print ("hello 5")
