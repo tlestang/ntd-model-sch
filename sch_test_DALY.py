0420b31384598280f177025429da8a74993465a9
Amanda Minter <amandalminter@gmail.com>
2022-04-27 12:24:50 +0100

    Testing run file
    File to run test parameters

--- sch_test_DALY.py
+++ sch_test_DALY.py
@@ -2,12 +2,10 @@ from sch_simulation.helsim_RUN_KK import SCH_Simulation_DALY
 
 
 # simulation with no vaccine (specified as coverage = 0)
-df0 = SCH_Simulation_DALY(paramFileName='SCH-high_adult_burden.txt', demogName='UgandaRural', numReps=1)
-df0.to_csv('SCH-high_adult_burden.csv')
+df0 = SCH_Simulation_DALY(paramFileName='test.txt', demogName='UgandaRural', numReps=1)
+df0.to_csv('SCH_test.csv')
 
 
-# simulation with no vaccine (specified as coverage = 0)
-#df0 = SCH_Simulation_DALY(paramFileName='SCH-low_adult_burden.txt', demogName='UgandaRural', numReps=1)
-#df0.to_csv('SCH-low_adult_burden.csv')
+
 
 
