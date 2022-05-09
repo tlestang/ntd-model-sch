d912cf287f383029c2e6b1e9e7c08c946ad573dc
Igor Clark <igor@igorclark.net>
2022-05-09 12:54:02 +0100

    add openpyxl dependency to setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 setuptools.setup(
     name='sch_simulation',
     version='0.0.1',
-    url='https://www.artrabbit.com/',
+    url='https://www.ntdmodelling.org',
     maintainer='ArtRabbit',
     maintainer_email='support@artrabbit.com',
     description='SCH simulation model',
     long_description='Individual-based model in Medley 1989 thesis and Anderson&Medley 1985.',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    install_requires=['numpy', 'scipy', 'pandas', 'joblib', 'matplotlib'],
+    install_requires=['numpy', 'scipy', 'pandas', 'joblib', 'matplotlib', 'openpyxl'],
     include_package_data=True
 )
