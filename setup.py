23e4357fb42f0e7a76a0a4f9391d7bdcc57afca4
Igor Clark <igor@igorclark.net>
2022-09-15 12:55:40 +0100

    fix pandas to v1.4.3 for gcs compatibility test

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setuptools.setup(
     long_description='Individual-based model in Medley 1989 thesis and Anderson&Medley 1985.',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    install_requires=['numpy', 'scipy', 'pandas', 'joblib', 'matplotlib', 'openpyxl'],
+    install_requires=['numpy', 'scipy', 'pandas==1.4.3', 'joblib', 'matplotlib', 'openpyxl'],
     include_package_data=True
 )
