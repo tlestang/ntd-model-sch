a8a768f3166eeed7f484a30fbd4bc9c4bfe3e457
Thomas Kiley <thomas.kiley@ndm.ox.ac.uk>
2024-06-04 13:19:08 +0100

    Limit supported versions to 3.10
    This is the version being used in the cloud runs. Later versions have issues with pkg_resource

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description='SCH simulation model',
     long_description='Individual-based model in Medley 1989 thesis and Anderson&Medley 1985.',
     packages=setuptools.find_packages(),
-    python_requires='>=3.9',
+    python_requires='>=3.9,<3.11',
     install_requires=['numpy', 'scipy', 'pandas', 'joblib', 'matplotlib', 'openpyxl', 'pytest'],
     include_package_data=True
 )
